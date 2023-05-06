7361c7af753fc9f7ad097b90fd810266914ff2a1
Sethuramalingam Duraisamy <59419619+Sethuramalingam-art@users.noreply.github.com>
2023-05-06 22:33:26 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 
 // const promise = new Promise((resolve, reject) => {});
 
-// then catch finally
+// then catch finally - ES5 methods
+
+// ASync await - ES6 methods
 
 // finaly will call only after then and catch
 
@@ -20,11 +22,49 @@ promise1.then((val) => {
   console.log(val);
 });
 
-var x = promise1
+promise1
   .then((val) => {
-    return "ABC" + val;
+    console.log("val1");
+    //return "ABC" + val;
   })
   .then((val) => {
-    console.log("val");
+    console.log("val2");
+    return "ABC" + val;
   });
-console.log(x);
+
+//result:
+//   Promise { <pending> }
+// Promise { <pending> }
+// hello world
+// val1
+// val2
+
+//Promie methods
+
+// Promise.all();
+// Promise.allSettled();
+// Promise.race();
+// Promise.any();
+
+//Using async,await promises in ES6 here we can use try catch methods
+
+// a function that declared with async it should return a promise
+
+const promise2 = new Promise((resolve, reject) =>
+  setTimeout(() => {
+    resolve("This is for ES6");
+  }, 5000)
+);
+
+async function example(promise) {
+  try {
+    const result = await promise;
+    console.log(result);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    console.log("finally block");
+  }
+}
+
+example(promise2);
